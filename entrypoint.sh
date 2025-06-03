c51cc1f6af15307d0174c6386ae3fa419a480b77
Palladium <138684256+Palladium-Innovations@users.noreply.github.com>
2025-06-03 08:59:52 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-# Start the SSM agent in the background
+# Ensure log directory exists with the right permissions
+mkdir -p /var/log/amazon/ssm
+chown -R root:root /var/log/amazon
+
+# Start SSM agent as root
 /usr/bin/amazon-ssm-agent &
+echo "Started amazon-ssm-agent"
 
-# Start your app (this will become PID 1)
-exec yarn start
+# Drop to non-root for your app
+exec su nodejs -c "yarn start"
